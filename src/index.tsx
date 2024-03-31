a9c8fb18a36dde88a3f8f23bf8f389e8cd6905f5
David Kim <tjdzms123@naver.com>
2024-03-31 16:45:11 +0900

    combinereducer 추가

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import counter from './reducers/counter';
+import rootReducer from './reducers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+const store = createStore(rootReducer);
+
+store.dispatch({
+  type: 'ADD_TODO',
+  text: 'Use Redux',
+});
+
+const render = () =>
+  root.render(
+    <React.StrictMode>
+      <App
+        value={store.getState()}
+        onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+        onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+      />
+    </React.StrictMode>
+  );
+render();
+store.subscribe(render);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
